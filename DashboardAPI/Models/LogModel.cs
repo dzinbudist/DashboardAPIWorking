e8337e7e8af7fa07c88474b7441fd8b320718ea0
dzemaitis <55211085+dzemaitis@users.noreply.github.com>
2020-01-23 19:17:55 -0800

    Connected db and added tables

---
+++ DashboardAPI/Models/LogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApi.Models
+{
+    public class LogModel
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Error_Code { get; set; }
+        public string Request_Type { get; set; }
+        public int Request_Ref { get; set; }
+    }
+}
