ba0bae1fd10370dcc7f16c755551d61bd1aa5490
Andrius V <52530715+dzinbudist@users.noreply.github.com>
2020-02-03 13:28:00 +0200

    Get metod in Ping Service

--- WebApi.Business/Services/PingService.cs
+++ WebApi.Business/Services/PingService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.NetworkInformation;
 using WebApi.Data.Data;
 using WebApi.Data.Entities;
@@ -18,6 +20,26 @@ namespace WebApi.Business.Services
             _context = context;
         }
 
+        async void GetService()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://www.kitm.lt/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var url = "http://www.kitm.lt/";
+            HttpResponseMessage response = await client.GetAsync("");
+            if (response.IsSuccessStatusCode)
+            {
+
+                url = await response.Content.ReadAsStringAsync();
+
+               //product = await response.Content.ReadAsAsync<Product>();
+            }
+        }
+
         public object PingDomainFromDb(int id)
         {
             var domainModel = _context.Domains.Find(id);
@@ -26,33 +48,40 @@ namespace WebApi.Business.Services
                 return null;
             }
             var hostname = domainModel.Url;
-            var domainPing = new Ping();
-            try
-            {
-                var reply = domainPing.Send(hostname);
-                if (reply != null && reply.Status == IPStatus.Success)
-                    return new
-                    {
-                        Url_Pinged = domainModel.Url, Status = reply.Status.ToString(), LatencyMS = reply.RoundtripTime
-                    };
-                domainModel.Last_Fail = DateTime.Now;
-                // new LogModel entity added to Database
-                var logEntry = new LogModel
-                {
-                    Domain_Id = domainModel.Id, Log_Date = DateTime.Now, Error_Text = reply.Status.ToString()
-                };
-                _context.Logs.Add(logEntry);
-                _context.SaveChanges();
 
-                return new
-                {
-                    Url_Pinged = domainModel.Url, Status = reply.Status.ToString(), LatencyMS = reply.RoundtripTime
-                };
-            }
-            catch
-            {
-                return null;
-            }
+            GetService();
+
+            return null; // sita istrint
+
+
+
+            //var domainPing = new Ping();
+            //try
+            //{
+            //    var reply = domainPing.Send(hostname);
+            //    if (reply != null && reply.Status == IPStatus.Success)
+            //        return new
+            //        {
+            //            Url_Pinged = domainModel.Url, Status = reply.Status.ToString(), LatencyMS = reply.RoundtripTime
+            //        };
+            //    domainModel.Last_Fail = DateTime.Now;
+            //    // new LogModel entity added to Database
+            //    var logEntry = new LogModel
+            //    {
+            //        Domain_Id = domainModel.Id, Log_Date = DateTime.Now, Error_Text = reply.Status.ToString()
+            //    };
+            //    _context.Logs.Add(logEntry);
+            //    _context.SaveChanges();
+
+            //    return new
+            //    {
+            //        Url_Pinged = domainModel.Url, Status = reply.Status.ToString(), LatencyMS = reply.RoundtripTime
+            //    };
+            //}
+            //catch
+            //{
+            //    return null;
+            //}
         }
     }
 
