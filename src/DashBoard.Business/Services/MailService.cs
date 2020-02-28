04671bcf73562f659f46f0d19acb7d9779e0656f
Andrius V <52530715+dzinbudist@users.noreply.github.com>
2020-02-28 09:09:44 +0200

    Mail update

--- src/DashBoard.Business/Services/MailService.cs
+++ src/DashBoard.Business/Services/MailService.cs
@@ -7,12 +7,16 @@ using DashBoard.Data.Entities;
 using DashBoard.Data.Data;
 using System.Threading.Tasks;
 using System.Linq;
+using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Newtonsoft.Json.Linq;
 
 namespace DashBoard.Business.Services
 {
     public interface IMailService
     {
         Task<bool> SendEmail(DomainModel domainModel, Guid teamKey, string responseCode);
+        Task<bool> SendEmailJet(DomainModel domainModel, Guid teamKey, string responseCode);
     }
     public class MailService: IMailService
     {
@@ -99,5 +103,120 @@ namespace DashBoard.Business.Services
             }
         }
 
+
+        public async Task<bool> SendEmailJet(DomainModel domainModel, Guid teamKey, string responseCode)
+        {
+            //double intervalMultiplier;
+            bool datePass = false;
+            bool notifiedPass = true;
+
+            //if (domainModel.Interval_Ms < 240000) //600000
+            //{
+            //    double intervalMultiplierDouble = 240000 / domainModel.Interval_Ms;
+            //    intervalMultiplier = Math.Round(intervalMultiplierDouble, MidpointRounding.AwayFromZero);
+            //}
+            //else
+            //{
+            //    intervalMultiplier = 1;
+            //}
+
+            var blackoutTime = DateTime.Now.AddMinutes(-5);//DateTime.Now.AddMilliseconds(-(domainModel.Interval_Ms * intervalMultiplier));
+            var logs = _context.Logs.Where(x => x.Domain_Id == domainModel.Id && x.Team_Key == teamKey && x.Log_Date >= blackoutTime).OrderBy(x => x.Log_Date).ToList();
+
+            if (logs.Count > 0)
+            {
+                datePass = logs.First().Log_Date.AddSeconds(-30) <= blackoutTime;
+
+                if (datePass)
+                {
+                    foreach (LogModel aModel in logs)
+                    {
+                        if (aModel.Notified)
+                        {
+                            notifiedPass = false;
+                        }
+                    }
+
+                    if (notifiedPass)
+                    {
+                        if (IsValidEmail(domainModel.Notification_Email))
+                        {
+                            MailjetClient client = new MailjetClient("a73620d74b8a7717f2ff320bcf7c47b0", "968e06d73b9d95c5ad79bdfee153051b")
+                            {
+                                Version = ApiVersion.V3_1,
+                            };
+                            MailjetRequest request = new MailjetRequest
+                            {
+                                Resource = Send.Resource,
+                            }
+                             .Property(Send.Messages, new JArray {
+                                 new JObject {
+                                  {
+                                   "From",
+                                    new JObject 
+                                    {
+                                        {"Email", "andrius.vr@gmail.com"},
+                                        {"Name", "WatchHound"}
+                                    }
+                                  }, {
+                                    "To",
+                                    new JArray 
+                                    {
+                                        new JObject 
+                                        {
+                                             {
+                                              "Email",
+                                              domainModel.Notification_Email
+                                             }, {
+                                              "Name",
+                                              ""
+                                             }
+                                         }
+                                   }
+                                  }, 
+                                     {
+                                       "Subject",
+                                       "Watch Hound - Something wrong with  " + domainModel.Service_Name
+                                  }, 
+                                     {
+                                       "TextPart",
+                                       ""
+                                  }, 
+                                     {
+                                       "HTMLPart",
+                                       "<p>Something wrong with</p>  <strong>" + domainModel.Service_Name + "</strong><br>" +
+                                              "<p>Endpoint:  </p><strong>" + domainModel.Url + "</strong><br>" + "<p>Error code:  </p><strong>" + responseCode + "</strong>"
+                        }, 
+                                     {
+                                       "CustomID",
+                                       "AppGettingStartedTest"
+                                  }
+                                 }
+                                   });                            
+
+                            MailjetResponse response = await client.PostAsync(request);
+
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var aLog = logs.Last();
+
+                                if (aLog != null)
+                                {
+                                    aLog.Notified = true;
+                                    _context.Logs.Update(aLog);
+                                    _context.SaveChanges();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
     }
 }
+
+
+
+
